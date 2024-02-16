9f7801193f700f45963f43f0b23f97f087ca6022
francoromeo <francoromeo5012@gmail.com>
2024-02-15 22:53:01 -0300

    php form

---
+++ php/form.php
@@ -0,0 +1,28 @@
+<?php
+$name=$_POST["name"];
+$email=$_POST["email"];
+$number=$_POST["number"];
+$message=$_POST["message"];
+$country=$_POST["country"];
+$city=$_POST["city"];
+$option=$_POST["option"];
+$company=$_POST["company"];
+$entrepreneur=$_POST["entrepreneur"];
+$cv=$_POST["cv"];
+$range=$_POST["range"];
+$date=$_POST["date"];
+$datedos=$_POST["date2"];
+echo"name: $name <br>";
+echo"email: $email <br>";
+echo"number: $number <br>";
+echo"Message: $message <br>";
+echo"country: $country <br>";
+echo"city: $city <br>";
+echo"option: $option <br>";
+echo"company: $company <br>";
+echo"entrepreneur: $entrepreneur <br>";;
+echo"cv: $cv <br>";
+echo"range: $range <br>";
+echo"date: $date <br>";
+echo"date2: $datedos <br>";
+?>
