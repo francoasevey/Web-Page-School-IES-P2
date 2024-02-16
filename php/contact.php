8c438002cfd2a5b70e2d160b04e97a56a463c13e
francoromeo <francoromeo5012@gmail.com>
2024-02-15 22:51:27 -0300

    php contact

---
+++ php/contact.php
@@ -0,0 +1,10 @@
+<?php
+$name=$_POST["name"];
+$email=$_POST["email"];
+$number=$_POST["number"];
+$textarea=$_POST["textarea"];
+echo"Message sent succesfully: $name <br>";
+echo"email: $email <br>";
+echo"number: $number <br>";
+echo"Message: $textarea <br>";
+?>
